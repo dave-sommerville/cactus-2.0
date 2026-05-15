588403067cddd56e12f4d823965dad93d3d589d1
Dave Sommerville <dave.r.sommerville@outlook.com>
2026-05-15 18:46:11 -0500

    Cast objects added

--- src/DAL/ShowList.js
+++ src/DAL/ShowList.js
@@ -2,31 +2,19 @@
 // Show One
 import Show from '../DAL/ShowModel';
 import {director, leadActor, cactusCastList, cactusCrewList } from '../DAL/PersonList';
+import cactusAllStaff from '../media/graphics/cast.jpg';
+import sssjgPoster from '../media/graphics/poster.jpg';
+import sssjgAllStaff from '../media/graphics/cast-photo.jpg';
 
 const castList = [leadActor];
 const crewList = [director];
 
-export const currentShow = new Show(
-  "Midnight in Winnipeg",            // title
-  "Elena Rossi",                     // author
-  "cover1.jpg",                      // coverPhoto 
-  "staff-group.jpg",                 // allStaffPhoto 
-  "A thrilling mystery set in MB.",  // synopsis
-  "Royal Manitoba Theatre Centre",   // location
-  [],                                // dateTimes
-  "https://tickets.example.com",     // ticketLink
-  "https://media.example.com/kit",   // mediaKitLink
-  null,                              // reviewsLink (Not yet available)
-  "",                                // galleryLink (Empty string becomes null)
-  castList,                          // castList (Array of Person objects)
-  crewList                           // crewList (Array of Person objects)
-);
 
 const Cactus = new Show(
   "Cactus",
   "Daniel Gilmour",
-  "",
-  "",
+  null,
+  cactusAllStaff,
   "When they're out of minions but a mortal needs to face their past before they die, sometimes the god of death must step in. It's just Tom's luck to both find out he's dying and to be escorted through his regrets by a surly deity who's out of practice. At least his best friend Jackie (and her trusty cactus, Angie) are coming along as emotional support. Written by Daniel Gilmour, who left the world's stage too soon. Cactus is a dark comedy about life, death, love and loss that we now perform in his memory.",
   "Venue 6 - Tom Hendry Warehouse",
   [
@@ -44,3 +32,6 @@ const Cactus = new Show(
   cactusCastList,
   cactusCrewList
 );
+
+
+export const currentShow = Cactus;
